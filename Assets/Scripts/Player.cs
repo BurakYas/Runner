49cb7b2edd7a5d5e4f7d44e3f4291acc970f8eb6
BurakYas <burakyasar749@gmail.com>
2024-10-07 14:51:22 +0300

    D3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    [Header("Speed info")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speedMultiplier;
+    [Space]
+    [SerializeField] private float milestoneIncreaser;
+    private float speedMilestone;
+
     [Header("Movement info")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
@@ -19,68 +26,122 @@ public class Player : MonoBehaviour
     [Header("Slide info")]
     [SerializeField] private float slideSpeed;
     [SerializeField] private float slideTime;
+    [SerializeField] private float slideCooldown;
+    private float slideCooldownCounter;
     private float slideTimeCounter;
     private bool isSliding;
 
     [Header("Collision info")]
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float ceillingCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private Vector2 wallCheckSize;
     private bool isGrounded;
     private bool wallDetected;
+    private bool ceillingDetected;
+    [HideInInspector] public bool ledgeDetected;
+
+    [Header("Ledge info")]
+    [SerializeField] private Vector2 offset1;
+    [SerializeField] private Vector2 offset2;
+
+    private Vector2 climbBegunPosition;
+    private Vector2 climbOverPosition;
+
+    private bool canGrabLedge = true;
+    private bool canClimb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        speedMilestone = milestoneIncreaser;
     }
 
     private void Update()
     {
-        ChechCollision();
+        CheckCollision();
         AnimatorControllers();
 
         slideTimeCounter -= Time.deltaTime;
+        slideCooldownCounter -= Time.deltaTime;
 
-        if (playerUnlocked && !wallDetected)
+        if (playerUnlocked)
             Movement();
 
         if (isGrounded)
             canDoubleJump = true;
 
+        SpeedController();
+
+        CheckForLedge();
         CheckForSlide();
         CheckInput();
     }
 
+    private void SpeedController()
+    {
+        if (moveSpeed == maxSpeed)
+            return;
+
+        if (transform.position.x > speedMilestone)
+        {
+            speedMilestone += milestoneIncreaser;
+
+            moveSpeed += speedMultiplier;
+            milestoneIncreaser += speedMilestone; // Increase the milestone
+
+            if (moveSpeed > maxSpeed)
+                moveSpeed = maxSpeed;
+        }
+    }
+
+    #region Ledge Climb Region
+    private void CheckForLedge()
+    {
+        if (ledgeDetected && canGrabLedge) // Check if the player is close to a ledge and can grab it
+        {
+            canGrabLedge = false;
+
+            Vector2 ledgePosition = GetComponentInChildren<LedgeDetection>().transform.position; // Get the position of the ledge detected
+
+            climbBegunPosition = ledgePosition + offset1; // Set the position where the player will start to climb
+            climbOverPosition = ledgePosition + offset2;
+
+            canClimb = true;
+        }
+
+        if (canClimb)
+            transform.position = climbBegunPosition; // Move the player to the climb begun position
+    }
+
+    private void LedgeClimbOver()
+    {
+        canClimb = false;
+        transform.position = climbOverPosition; // Move the player to the climb over position
+        Invoke("AllowLedgeGrab", 1f); // Allow the player to grab the ledge again after 0.5 seconds
+    }
+
+    private void AllowLedgeGrab() => canGrabLedge = true;
+
     private void CheckForSlide()
     {
-        if (slideTimeCounter < 0)
+        if (slideTimeCounter < 0 && !ceillingDetected)
             isSliding = false;
     }
+    #endregion
 
     private void Movement()
     {
+        if (wallDetected)
+            return;
+
         if (isSliding)
             rb.velocity = new Vector2(slideSpeed, rb.velocity.y); // Slide the player
         else
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y); // Move the player
-    }
-
-    private void AnimatorControllers()
-    {
-        anim.SetFloat("xVelocity", rb.velocity.x); // Set the x velocity for the run animation
-        anim.SetFloat("yVelocity", rb.velocity.y); // Set the y velocity for the jump animation
-
-        anim.SetBool("canDoubleJump", canDoubleJump);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isSliding", isSliding);
-    }
-
-    private void ChechCollision()
-    {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround); // Check if the player is grounded
-        wallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, whatIsGround); // Check if the player is touching a wall
     }
 
     private void CheckInput()
@@ -98,15 +159,19 @@ public class Player : MonoBehaviour
 
     private void SlideButton()
     {
-        if (rb.velocity.x != 0)
+        if (rb.velocity.x != 0 && slideCooldownCounter < 0)
         {
             isSliding = true;
             slideTimeCounter = slideTime;
+            slideCooldownCounter = slideCooldown; // Set the slide cooldown counter
         }
     }
 
     private void JumpButton()
     {
+        if (isSliding)
+            return;
+
         if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -118,9 +183,28 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void AnimatorControllers()
+    {
+        anim.SetFloat("xVelocity", rb.velocity.x); // Set the x velocity for the run animation
+        anim.SetFloat("yVelocity", rb.velocity.y); // Set the y velocity for the jump animation
+
+        anim.SetBool("canDoubleJump", canDoubleJump);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isSliding", isSliding);
+        anim.SetBool("canClimb", canClimb);
+    }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround); // Check if the player is grounded
+        ceillingDetected = Physics2D.Raycast(transform.position, Vector2.up, ceillingCheckDistance, whatIsGround); // Check if the player is touching a ceilling
+        wallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, whatIsGround); // Check if the player is touching a wall
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance)); // Draw a line to show the ground check distance
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + ceillingCheckDistance)); // Draw a line to show the ceilling check distance
         Gizmos.DrawWireCube(wallCheck.position, wallCheckSize); // Draw a wire cube to show the wall check size
     }
 }
